c57afe62c5e83b9bc51281c5d5d8522a54ccf234
RyoOzaki <ryo.ozaki@em.ci.ritsumei.ac.jp>
2018-09-26 02:01:33 -0700

    updated

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ with open('MIT-LICENSE.txt') as f:
     license = f.read()
 
 setup(
-    name='SparseAutoencoder',
+    name='DSAE_PBHL',
     version='0.0.1',
-    description='Package of the sparse autoencoder',
+    description='Package of the sparse autoencoder (SAE), deep SAE (DASE), SAE with parametric bias in hidden layer (SAE with PBHL), and DSAE with PBHL',
     long_description=readme,
     author='Ryo Ozaki',
     author_email='ryo.ozaki@em.ci.ritsumei.ac.jp',
     url='https://github.com/RyoOzaki/SparseAutoencoder',
     license=license,
     install_requires=['numpy', 'tensorflow'],
-    packages=find_packages(exclude=('SparseAutoencoder','SparseAutoencoder.util'))
+    packages=find_packages(exclude=('DSAE_PBHL','DSAE_PBHL.util'))
 )

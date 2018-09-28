65a57e304cfea3407ba0a19d994ea1b4426ef767
RyoOzaki <ryo.ozaki@em.ci.ritsumei.ac.jp>
2018-09-27 17:12:48 -0700

    updated the normalizer

--- DSAE_PBHL/util/normalizer.py
+++ DSAE_PBHL/util/normalizer.py
@@ -7,23 +7,23 @@ class Normalizer(object):
             max_v = np.array(max_v)
         if min_v is not None:
             min_v = np.array(min_v)
-        self.params = {"max_v": max_v, "min_v": min_v}
+        self._params = {"max_v": max_v, "min_v": min_v}
 
     @property
     def max_v(self):
-        return self.params["max_v"]
+        return self._params["max_v"]
 
     @max_v.setter
     def max_v(self, v):
-        self.params["max_v"] = np.array(v)
+        self._params["max_v"] = np.array(v)
 
     @property
     def min_v(self):
-        return self.params["min_v"]
+        return self._params["min_v"]
 
     @min_v.setter
     def min_v(self, v):
-        self.params["min_v"] = np.array(v)
+        self._params["min_v"] = np.array(v)
 
     def normalize(self, data):
         if self.max_v is None:
@@ -42,13 +42,13 @@ class Normalizer(object):
         return unnormalized
 
     def save_params(self, p):
-        np.savez(f, **self.params)
+        np.savez(f, **self._params)
 
     def load_params(self, f):
         params = np.load(f)
         if "max_v" not in params or "min_v" not in params:
             raise RuntimeError("Parameters are not initialized.")
-        self.params = params
+        self._params = params
 
     @classmethod
     def load(cls, f):

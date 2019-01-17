d0df288fd45be930bca7ac2e82ca6af0a9d29e07
RyoOzaki <ryo.ozaki@em.ci.ritsumei.ac.jp>
2019-01-17 01:31:28 -0800

    removed a merge indicaters

--- setup.py
+++ setup.py
@@ -9,11 +9,7 @@ with open('MIT-LICENSE.txt') as f:
 
 setup(
     name='DSAE_PBHL',
-<<<<<<< HEAD
-    version='1.0.0',
-=======
     version='1.0.2',
->>>>>>> develop
     description='Package of the sparse autoencoder (SAE), deep SAE (DASE), SAE with parametric bias in hidden layer (SAE with PBHL), and DSAE with PBHL',
     long_description=readme,
     author='Ryo Ozaki',
